3151089fc24e583157591ad41cb70de7deba7070
Evan1oconto <22307805+Evan1oconto@users.noreply.github.com>
2020-09-07 19:29:05 -0400

    modified a couple sentences

--- src/react_Components/about.jsx
+++ src/react_Components/about.jsx
@@ -21,14 +21,10 @@ export default class About extends Component {
                                     <div className="col-md-12 heading-section">
                                         <h1 className="big">About</h1>
                                         <h2 className="mb-4">About Me</h2>
-                                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                                        <p>I am a sophmore at Tufts University that is interested in computer science. I enjoy watching and playing sports, learning about the stock market, and rock climbing</p>
                                         <ul className="about-info mt-4 px-md-0 px-2">
                                             <li className="d-flex"><span>Name:</span> <span>Evan L</span></li>
-                                            <li className="d-flex"><span>Date of birth:</span> <span>November 28, 1989</span></li>
-                                            <li className="d-flex"><span>Address:</span> <span>27 Mosswood Circle, Amherst NH</span></li>
-                                            <li className="d-flex"><span>Zip code:</span> <span>03031</span></li>
-                                            <li className="d-flex"><span>Email:</span> <span>evanloconto@gmail.com</span></li>
-                                            <li className="d-flex"><span>Phone: </span> <span>603-714-3726</span></li>
+                                            <li className="d-flex"><span>Address:</span> <span>Amherst NH</span></li>
                                         </ul>
                                     </div>
                                 </div>
